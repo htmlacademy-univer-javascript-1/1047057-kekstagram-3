cc69dc48fe96ccef2f919f799a2d2bef5393b62d
Ivan Voronov <10raven11@gmail.com>
2023-03-11 04:22:52 +0300

    module 4 task 1 fixes

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ function isBelowMaxStringLength(string, maxLength) {
 }
 
 function generateImgDescriptionArray() {
-  let imgDescriptions = new Array(25);
-  for(i = 1; i <= 25; i++) {
-    imgDescriptions[i - 1] = {id: i, url: "photos/" + i +".jpg",
-    description: "I rate this picture " + getRandomFromInterval(0, 10) + " outta 10",
-    likes: getRandomFromInterval(15, 200), comments: getRandomFromInterval(0, 200),}
+  const imgDescriptions = new Array(25);
+  for(let i = 1; i <= 25; i++) {
+    imgDescriptions[i - 1] = {id: i, url: `photos/${i}.jpg`,
+      description: `I rate this picture ${getRandomFromInterval(0, 10)} outta 10`,
+      likes: getRandomFromInterval(15, 200), comments: getRandomFromInterval(0, 200),};
   }
-  console.log(imgDescriptions);
   return imgDescriptions;
 }
 
+isBelowMaxStringLength();
 generateImgDescriptionArray();
